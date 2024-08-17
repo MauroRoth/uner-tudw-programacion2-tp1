77c226d839663170a357e6309546ffa2a8dbbed6
Mauro Roth <maurogroth@gmail.com>
2024-08-17 19:51:30 -0300

    cuarto commit: se empezó a hacer 3c, sobre expresiones de lógica proposiconal

---
+++ 3c.py
@@ -0,0 +1,20 @@
+# Tal como sucede con la lógica proposicional, en Python muchas veces las
+# expresiones booleanas pueden ser simplificadas manteniendo el valor de
+# verdad de la expresión. Así, por ejemplo, (a and b) or (b and a) es equivalente
+# a a and b. A continuación, intente simplificar las siguientes expresiones y
+# escriba un procedimiento procesar_sentencias(a, b, c) que permita evaluar el
+# valor de verdad de las expresiones ya simplificadas:
+# i. (a or b) or (b and c)
+# ii. b and c or False
+# iii. a and b or c or (b and a)
+# iv. a == True or b == False
+
+def procesar_sentencias(a,b,c = True):
+    sentencia0 = a and b # (a and b) or (b and a)
+    sentencia1 = a or b # (a or b) or (b and c)
+    sentencia2 = b and c # b and c or False
+    sentencia3 = a and b or c # a and b or c or (b and a)
+    sentencia4 = True # a == True or b == False
+    print(sentencia0)
+
+procesar_sentencias(False,True)
